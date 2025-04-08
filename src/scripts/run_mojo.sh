ca5f8fa3d965f4ce6eb0c42b5d4b458e01c59532
Aidan Dyga <aidan.dyga@gmail.com>
2025-04-08 11:48:31 -0400

    fix: repo refactoring changes

--- src/scripts/run_mojo.sh
+++ src/scripts/run_mojo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Navigate to the optimizer directory
+magic init optimizer --format mojoproject
 cd optimizer
 magic run mojo main.mojo
--- src/train/mojo_train.py
+++ src/train/mojo_train.py
@@ -8,5 +8,5 @@ mojo main.mojo
 import subprocess
 
 def run_mojo():
-    subprocess.run(["bash", "src/train/scripts/run_mojo.sh"], check=True)
+    subprocess.run(["bash", "src/scripts/run_mojo.sh"], check=True)
     # run the gru_model.py somehow with the mojo's magic compiler
