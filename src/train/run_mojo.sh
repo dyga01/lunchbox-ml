058451f93cb7d29c0cd46d4749c08b470fb9b77d
Aidan Dyga <aidan.dyga@gmail.com>
2025-04-07 22:24:33 -0400

    feat: working shell script to run mojo program

--- src/train/run_mojo.sh
+++ src/train/run_mojo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Navigate to the optimizer directory
-cd optimizer || exit
-
-# Use magic exec to run the Mojo command in the environment
-magic exec mojo main.mojo
+cd optimizer
+magic run mojo main.mojo
